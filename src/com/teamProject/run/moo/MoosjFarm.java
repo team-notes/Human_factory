cf83b609fe82bb992299f52ec6469f7ca68be0b3
Izac626 <dkdlwkr207@gmail.com>
2026-03-05 17:01:15 +0900

    음무!!!!!!!! 안쓰는거 제거 버전 음무!!!!!!!

--- src/com/teamProject/run/moo/MoosjFarm.java
+++ src/com/teamProject/run/moo/MoosjFarm.java
@@ -6,14 +6,10 @@ public class MoosjFarm <T extends Moosj>{
     public MoosjFarm(T Msk){
         this.Msk = Msk;
     }
-    public MoosjFarm(){
-    }
 
     public T getMsk() {
         return Msk;
     }
 
-    public void setMsk(T msk) {
-        this.Msk = msk;
-    }
+
 }
